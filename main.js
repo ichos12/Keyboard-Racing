95b0d1038c8f65459b0d18765638cc89c4b23db0
Ichos12 <magus78@yandex.ru>
2015-02-08 01:05:21 +0300

    Теперь при вводе пробела в строке, слово исчезает.

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 /**
  * Created by Максим on 06.02.2015.
  */
-$(document).ready ( function()
-    {
-        $(".input").keyup(function()
-        {
-            $('#contentInput').text($(".input").val());
-        });
-    });
-/*function if_space()
-{
-    if (contentInput = $('#contentInput').val(' ')) {
+function getChar(event) {
+    if (event.which == null) {
+        if (event.keyCode < 32) return null;
+        return String.fromCharCode(event.keyCode)
     }
-        $('#contentInput').val('');
+
+    if (event.which!=0 && event.charCode!=0) {
+        if (event.which < 32) return null;
+        return String.fromCharCode(event.which);
     }
+
+    return null;
 }
 
+function if_space(event) {
+    if (event.keyCode == 32) document.getElementById('input').value = '';
+}
