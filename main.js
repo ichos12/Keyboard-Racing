c57d25780d5fe935761c2d84cba5affd0627f03f
Ichos12 <magus78@yandex.ru>
2015-02-11 16:51:23 +0300

    Было поправлено форматирование

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 /**
  * Created by Максим on 06.02.2015.
  */
-function getChar(event) {
-    if (event.which == null) {
-        if (event.keyCode < 32) return null;
+function getChar(event)
+{
+    if (event.which == null)
+    {
+        if (event.keyCode < 32)
+        {
+            return null;
+        }
         return String.fromCharCode(event.keyCode)
     }
 
-    if (event.which!=0 && event.charCode!=0) {
-        if (event.which < 32) return null;
+    if (event.which!=0 && event.charCode!=0)
+    {
+        if (event.which < 32)
+        {
+            return null;
+        }
         return String.fromCharCode(event.which);
     }
 
     return null;
 }
 
-function if_space(event) {
-    if (event.keyCode == 32) document.getElementById('input').value = '';
+function ifSpace(event)
+{
+    if (event.keyCode == 32)
+    {
+        document.getElementById('input').value = '';
+    }
 }
